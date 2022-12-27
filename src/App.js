97020968920efed75fd71fe1b84e72c5b61a04c5
leejunmoo <dlquddnr122@naver.com>
2022-12-27 15:20:32 +0900

    리액트 연습1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>리액트 APP</h1>
+      <p>리액트 처음 시작합니다.</p>
+      <Hello></Hello>
     </div>
+    
   );
+}
+
+// Hello 함수(컴포넌트) => JSX 태그
+function Hello() {
+  return  (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
 }
 
 export default App;
